581685c02c4c304dbcfcded024e938574051d141
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2012-10-14 02:29:05 +0900

    add movielens example, ported from https://github.com/odasatoshi/movielens

---
+++ ml_analysis.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -Ilib/
+
+use strict;
+use warnings;
+use utf8;
+
+use Jubatus::Recommender::Client;
+
+my $NAME = "recommender_ml";
+
+my $r = new Jubatus::Recommender::Client("127.0.0.1", 9199);
+
+for (my $i = 0; $i < 943; $i++) {
+    my $sr = $r->similar_row_from_id($NAME, $i."", 10);
+    print "user ", $i, " is similar to: ";
+    for (my $n = 1; $n < @$sr; $n++) {
+        print $$sr[$n][0], ", ";
+    }
+    print "\n";
+}
---
+++ ml_update.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -Ilib/
+
+use strict;
+use warnings;
+use utf8;
+use Fcntl;
+
+use Jubatus::Recommender::Client;
+use Jubatus::Recommender::ConfigData;
+use Jubatus::Recommender::Datum;
+
+my $NAME = "recommender_ml";
+
+my $r = new Jubatus::Recommender::Client("127.0.0.1", 9199);
+
+my $converter = get_converter();
+
+my $config = new Jubatus::Recommender::ConfigData("lsh", $converter);
+$r->set_config($NAME, $config);
+
+sysopen (my $fh, "./dat/ml-100k/u.data", O_RDONLY) || die "cannot open data file";
+
+my $d = new Jubatus::Recommender::Datum();
+my $n = 0;
+while (<$fh>) {
+    my ($userid, $movieid, $rating, $mtime) = split;
+    my $num_values = [];
+    if ($n % 1000 == 0) {
+        print $n, "\n";
+    }
+    push @$num_values, [$movieid, $rating/1.0];
+    $d->{'num_values'} = $num_values;
+    $r->update_row($NAME, $userid, $d);
+    $n++;
+}
+close $fh;
+
+
+sub get_converter { local $/; return <DATA>; }
+
+__DATA__
+{
+  "string_filter_types": {},
+  "string_filter_rules": [],
+  "num_filter_types": {},
+  "num_filter_rules": [],
+  "string_types": {},
+  "string_rules": [],
+  "num_types": {},
+  "num_rules": [{"key" : "*", "type" : "num"}]
+}
